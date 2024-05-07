58725db3d83a8db320374c962b7f0792f46e499a
szigetvarikati <szigetvarikati@gmail.com>
2024-05-07 13:38:50 +0200

    add cypress.config.js

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://restful-booker.herokuapp.com',
+    setupNodeEvents(on, config) {},
+    viewportHeight: 550,
+    viewportWidth: 660,
+    //this is a recorder for the cypress, what i do in the test browser, it will recorded
+    experimentalStudio: true,
+  },
+});
