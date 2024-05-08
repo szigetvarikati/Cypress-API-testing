17c36736b37d80d39e44ce34b27778ded1048d31
szigetvarikati <szigetvarikati@gmail.com>
2024-05-08 14:55:49 +0200

    change baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://jsonplaceholder.typicode.com',
+    baseUrl:
+      'https://my-json-server.typicode.com/szigetvarikati/Cypress-API-testing',
     //this is a recorder for the cypress, what i do in the test browser, it will recorded
     experimentalStudio: true,
+    supportFile: false,
   },
 });
